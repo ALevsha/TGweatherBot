da8c5fcc57a3954264f1b4fe7b9239d76c96931a
ALevsha <levsha26r@gmail.com>
2023-03-10 15:37:57 +0300

    add repositories

---
+++ src/main/kotlin/data/remote/repository/WeatherRepository.kt
@@ -0,0 +1,26 @@
+package data.remote.repository
+
+import data.remote.api.ReversedGeocodingApi
+import data.remote.api.WeatherApi
+import data.remote.models.CurrentWeather
+import data.remote.models.ReversedCountry
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class WeatherRepository(
+    private val weatherApi: WeatherApi,
+    private val reversedGeocodingApi: ReversedGeocodingApi
+) {
+
+    suspend fun getCurrentWeather(apiKey: String, countryName: String, airQualityData: String): CurrentWeather{
+        return withContext(Dispatchers.IO){
+            weatherApi.getCurrentWeather(apiKey, countryName, airQualityData)
+        }.await()
+    }
+
+    suspend fun getReverseGeocodingCountryName(latitude: String, longitude: String, format: String): ReversedCountry{
+        return withContext(Dispatchers.IO){
+            reversedGeocodingApi.getCountryName(latitude, longitude, format)
+        }.await()
+    }
+}
