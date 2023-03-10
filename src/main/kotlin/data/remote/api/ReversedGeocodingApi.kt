11f21c4c311c71f6600e96675f42fbf52879e87f
ALevsha <levsha26r@gmail.com>
2023-03-10 14:04:06 +0300

    add api models

--- src/main/kotlin/data/remote/api/ReversedGeocodingApi.kt
+++ src/main/kotlin/data/remote/api/ReversedGeocodingApi.kt
@@ -1,10 +1,11 @@
 package data.remote.api
 
+import data.remote.models.ReversedCountry
 import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface ReverseGeocodingApi {
+interface ReversedGeocodingApi {
 
     @GET("reverse")
     fun getCurrentWeather(
@@ -12,7 +13,7 @@ interface ReverseGeocodingApi {
         @Query("lat") latitude: String, // широта
         @Query("lon") longitude: String, // долгота
         @Query("format") formatData: String // формат получения данных (gson)
-    ): Deferred<> // Спец тип от корутин. Указывается тип модели, которая приходит с сервера
+    ): Deferred<ReversedCountry> // Спец тип от корутин. Указывается тип модели, которая приходит с сервера
 }
 
 
