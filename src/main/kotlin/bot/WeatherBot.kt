e57abf936c64d1a27caa0962233b75228fbcac19
ALevsha <levsha26r@gmail.com>
2023-03-15 14:34:40 +0300

    add bot body

--- src/main/kotlin/bot/WeatherBot.kt
+++ src/main/kotlin/bot/WeatherBot.kt
@@ -4,8 +4,12 @@ import com.github.kotlintelegrambot.Bot
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
 import com.github.kotlintelegrambot.dispatcher.command
+import com.github.kotlintelegrambot.dispatcher.text
 import com.github.kotlintelegrambot.entities.ChatId
+import com.github.kotlintelegrambot.entities.InlineKeyboardMarkup
+import com.github.kotlintelegrambot.entities.keyboard.InlineKeyboardButton
 import data.remote.repository.WeatherRepository
+import jdk.jfr.internal.LogLevel
 
 private const val BOT_ANSWER_TIMEOUT = 30 // timeout, в котором бот может дать ответ
 private const val BOT_TOKEN = "5975392537:AAFRa5MjfJOwNUkdAwh27pQo7ivRDKr7_x4" // в идеале выносится на отдельный сервер
@@ -14,11 +18,12 @@ private const val BOT_TOKEN = "5975392537:AAFRa5MjfJOwNUkdAwh27pQo7ivRDKr7_x4" /
 class WeatherBot(private val weatherRepository: WeatherRepository) {
 
     private var _chatId: ChatId? = null
-    private val chatId: ChatId get() = requireNotNull(_chatId) // выкидывает значение, если не null иначе Exception
+    private val chatId by lazy { requireNotNull(_chatId)} // выкидывает значение, если не null иначе Exception
     fun createBot(): Bot{
         return bot {
             timeout = BOT_ANSWER_TIMEOUT
             token = BOT_TOKEN
+            logLevel = com.github.kotlintelegrambot.logging.LogLevel.Error
 
             dispatch {// внутри этой функции происходит ответ на запросы пользователя
                 setUpCommands()
@@ -33,6 +38,29 @@ class WeatherBot(private val weatherRepository: WeatherRepository) {
                 chatId = chatId,
                 text = "Привет, я бот, который отображает погоду! \n Для запуска бота введи команду /weather"
                 )
+        }
+
+        command("weather"){
+            val inlineKeyboardMarkup = InlineKeyboardMarkup.create(
+                listOf(
+                    InlineKeyboardButton.CallbackData(
+                        text = "Определить мой город (для мобильных устройств)",
+                        callbackData = "getMyLocation"
+                    )
+                ),
+                listOf(
+                    InlineKeyboardButton.CallbackData(
+                        text = "Ввести город вручную",
+                        callbackData = "enterManually"
+                    )
+                )
+            )
+
+            bot.sendMessage(
+                chatId = chatId,
+                text = "Для того, чтобы я смог отправить тебе погоду, \n мне нужно знать твой город.",
+                replyMarkup = inlineKeyboardMarkup
+            )
         }
     }
 }
